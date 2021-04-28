88c18c8aae82830b24be502146119624cc5c237d
Stefano-Para <stefano.zina@gmail.com>
2021-04-28 14:08:07 +0200

    esercizio riuscito ma dubbio sul riconoscimento

--- js/script.js
+++ js/script.js
@@ -6,15 +6,21 @@ console.log(richiestaEmail);
 
 
 // lista email verificate 
-var verifiedEmails = "uno";
+var verifiedEmails = ["uno@email.it", "due@email.it", "tre@email.it"];
 console.log(verifiedEmails);
 
 // controlla che sia nella lista di chi può accedere
 
-if (richiestaEmail == verifiedEmails) {
+if (verifiedEmails.includes(richiestaEmail)) {
     alert("Congratulazioni, puoi registrarti.")
 } else {
     alert("ERRORRE: email non riconosciuta")
 }
-
 // stampa un messaggio appropriato sull'esito del controllo.
+
+
+
+// come faccio a fare in modo che solo gli input inseriti che abbiano @qualcosa.it siano riconosciuti?
+
+// esempio: XXX@email.it è riconosciuta
+//          XXX@prova.com non è riconosciuta
