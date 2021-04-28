948a1e4dedc1ce1eed45528f9002634dfd0d2726
Stefano-Para <stefano.zina@gmail.com>
2021-04-28 16:15:07 +0200

    ex ricorretto

--- js/script.js
+++ js/script.js
@@ -6,21 +6,37 @@ console.log(richiestaEmail);
 
 
 // lista email verificate 
-var verifiedEmails = ["uno@email.it", "due@email.it", "tre@email.it"];
+var verifiedEmails = ["uno@email.it", "due@email.it", "tre@email.it", "quattro@email.it", "cinque@email.it"];
 console.log(verifiedEmails);
 
 // controlla che sia nella lista di chi può accedere
 
-if (verifiedEmails.includes(richiestaEmail)) {
-    alert("Congratulazioni, puoi registrarti.")
-} else {
-    alert("ERRORRE: email non riconosciuta")
+
+var listed = false;
+for (var i = 0; i < verifiedEmails.length; i++) {
+    if (richiestaEmail == verifiedEmails[i]){
+        listed = true;
+    }
 }
-// stampa un messaggio appropriato sull'esito del controllo.
 
+if (listed == true) {
+    var message = "Congratulazioni! Ti sei registrato."
+    document.getElementById("stamp").innerHTML = message;
+} else {
+    var message = "Errore: accesso negato."
+    document.getElementById("stamp").innerHTML = message;
+}
 
+/* stampa un messaggio appropriato sull'esito del controllo.
 
-// come faccio a fare in modo che solo gli input inseriti che abbiano @qualcosa.it siano riconosciuti?
+ !!! QUESTO METODO FUNZIONA MA BYPASSA IL FOR 
+ if (verifiedEmails.includes(richiestaEmail)) {
+     alert("Congratulazioni, puoi registrarti.")
+ } else {
+     alert("ERRORRE: email non riconosciuta")
+ }
 
-// esempio: XXX@email.it è riconosciuta
-//          XXX@prova.com non è riconosciuta
+ come faccio a fare in modo che solo gli input inseriti che abbiano @qualcosa.it siano riconosciuti?
+
+ esempio: XXX@email.it è riconosciuta
+          XXX@prova.com non è riconosciuta */
